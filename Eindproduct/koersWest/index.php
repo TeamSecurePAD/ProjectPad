5b12ee7b9f7d06aa546784c6b94ac3de6bb01d24
Gijs Sickenga <gijs.sickenga@hva.nl>
2016-04-20 22:10:12 +0200

    Indentation en comments aangepast voor betere leesbaarheid

--- Eindproduct/koersWest/index.php
+++ Eindproduct/koersWest/index.php
@@ -1,61 +1,55 @@
 <?php
+  session_start();
+  require 'databaseConnectionOpening.php';
 
-session_start();
-require 'databaseConnectionOpening.php';
+  if (isset($_SESSION['user_id']))
+  {
+    $id = $_SESSION['user_id'];
 
-if (isset($_SESSION['user_id'])){
+    $query = "SELECT id, email, straat, telefoonnummer FROM gebruiker WHERE id = $id";
 
-   $id = $_SESSION['user_id'];
+    $result= mysqli_query($connection, $query);
 
-   $query = "SELECT id, email, straat, telefoonnummer FROM gebruiker WHERE id = $id";
-
-  $result= mysqli_query($connection, $query);
-
-  if ($result) {
-      $row = mysqli_fetch_row($result);
-      $gebruikerID = $row[0];
-      $dbemail = $row[1];
-      $dbstraat = $row[2];
-      $dbtelnummer = $row[3];
-
+    if ($result)
+    {
+        $row = mysqli_fetch_row($result);
+        $gebruikerID = $row[0];
+        $dbemail = $row[1];
+        $dbstraat = $row[2];
+        $dbtelnummer = $row[3];
+    }
   }
-}
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/styles.css">
-  <title>welcome</title>
-</head>
-<body>
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>welcome</title>
+  </head>
 
-  <?php if ( isset($_SESSION['user_id'])): ?>
+  <body>
+    <?php if ( isset($_SESSION['user_id'])): ?>
 
-  <h1>Uw gegevens</h1>
-  <p><?php echo($dbemail) ?></p>
-  <p><?php echo($dbstraat); ?></p>
-  <p><?php echo($dbtelnummer) ?></p>
-  
- 
+    <h1>Uw gegevens</h1>
+    <p><?php echo($dbemail) ?></p>
+    <p><?php echo($dbstraat); ?></p>
+    <p><?php echo($dbtelnummer) ?></p>
+    
+    <a href="logout.php">Uitloggen</a>
 
-  <a href="logout.php">Uitloggen</a>
+    <?php else: ?>
+    	  <h1>welcome</h1>
+    	  <a href="login.php">Login</a>
+    	  <a href="register.php">Register</a>
+    <?php endif; ?>
 
-<?php else: ?>
-
-	  <h1>welcome</h1>
-	  <a href="login.php">Login</a>
-	  <a href="register.php">Register</a>
-
-<?php endif; ?>
-
-<script src="js/jquery-2.1.4.min.js"></script>
-<script src="js/bootstrap.min.js"></script> 
-<script src="js/script.js"></script>
-</body>
+    <script src="js/jquery-2.1.4.min.js"></script>
+    <script src="js/bootstrap.min.js"></script> 
+    <script src="js/script.js"></script>
+  </body>
 </html>
--- Eindproduct/koersWest/register.php
+++ Eindproduct/koersWest/register.php
@@ -1,71 +1,74 @@
 <?php
-session_start();
+	session_start();
 
+	if ( isset($_SESSION['user_id']))
+	{
+	  header('location:index.php');
+	}
 
-if ( isset($_SESSION['user_id'])){
-  header('location:index.php');
-}
+	require 'databaseConnectionOpening.php';
 
-require 'databaseConnectionOpening.php';
+	if (!empty($_POST['email']) && !empty($_POST['wachtwoord']))
+	{
+		// Save form data in temporary variables
+		$email = $_POST['email'];
+		$wachtwoord = $_POST['wachtwoord'];
+		$telnummer = $_POST['telnummer'];
+		$straat = $_POST['straat'];
+		$postcode = $_POST['postcode'];
+		$woonplaats = $_POST['woonplaats'];
 
-if (!empty($_POST['email']) && !empty($_POST['wachtwoord'])) {
+		// Insert our form data into the database
+		$query = "INSERT INTO gebruiker (email, wachtwoord, telefoonnummer, straat, postcode, woonplaats)
+			  	  VALUES ('$email', '$wachtwoord', '$telnummer', '$straat', '$postcode', '$woonplaats')";
 
-	//hier zet ik de form data in varriable
-	$email = $_POST['email'];
-	$wachtwoord = $_POST['wachtwoord'];
-	$telnummer = $_POST['telnummer'];
-	$straat = $_POST['straat'];
-	$postcode = $_POST['postcode'];
-	$woonplaats = $_POST['woonplaats'];
+		$result = mysqli_query($connection, $query);
 
-// zet de data uit het register form in een database.
-$query = "INSERT INTO gebruiker (email, wachtwoord, telefoonnummer, straat, postcode, woonplaats)
-		  VALUES ('$email', '$wachtwoord', '$telnummer', '$straat', '$postcode', '$woonplaats' )";
-
-		  $result = mysqli_query($connection, $query);
-
-if ($result) {
-	echo "U staat geregistreerd bij boot.";
+		// Successful registration
+		if ($result)
+		{
+			echo "U staat geregistreerd bij boot.";
 		}
-else
-{
- 	die ("Database query failed". mysql_error($connection));
-}
-
-}
-
+		else // Failed registration
+		{
+		 	die ("Database query failed". mysql_error($connection));
+		}
+	}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/styles.css">
-  <title>register</title>
-</head>
-<body>
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/styles.css">
+		<title>register</title>
+	</head>
 
-<form action="register.php" method="POST">
+	<body>
+		<!-- Registration form -->
+		<form action="register.php" method="POST">
 
-		<input type="text" placeholder="email" name="email">
+				<input type="text" placeholder="email" name="email">
 
-		<input type"password" placeholder="wachtwoord" name="wachtwoord">
-		<input type"password" placeholder="wachtwoord" name="herhaal_wachtwoord">
+				<input type"password" placeholder="wachtwoord" name="wachtwoord">
+				<input type"password" placeholder="wachtwoord" name="herhaal_wachtwoord">
 
-		<input type="text" placeholder="tel. nummer" name="telnummer">
-		<input type="text" placeholder="straat" name="straat">
-		<input type="text" placeholder="postcode" name="postcode">
-		<input type="text" placeholder="woonplaats" name="woonplaats">
+				<input type="text" placeholder="tel. nummer" name="telnummer">
+				<input type="text" placeholder="straat" name="straat">
+				<input type="text" placeholder="postcode" name="postcode">
+				<input type="text" placeholder="woonplaats" name="woonplaats">
 
-		<input type="submit">
+				<input type="submit">
 
-	</form>
+		</form>
 
-<script src="js/jquery-2.1.4.min.js"></script>
-<script src="js/bootstrap.min.js"></script> or
-<script src="js/script.js"></script>
-</body>
+		<a href="index.php">Terug</a>
+
+		<script src="js/jquery-2.1.4.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/script.js"></script>
+	</body>
 </html>
