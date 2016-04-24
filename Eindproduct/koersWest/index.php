46b7dca4384adfd329d70045c25dd8ee12e9cf28
RikLangeveld <rik_langeveld@hotmail.com>
2016-04-24 23:20:12 +0200

    Added matching functionaliteit!
    Hele grote Update! geniet ervan

--- Eindproduct/koersWest/index.php
+++ Eindproduct/koersWest/index.php
@@ -1,4 +1,4 @@
-<?php
+  <?php
   session_start();
 
   require 'databaseConnectionOpening.php';
@@ -15,17 +15,34 @@
 
     if ($result)
     {
-        $row = mysqli_fetch_row($result);
-        $gebruikerID = $row[0];
-        $dbemail = $row[1];
-        $dbstraat = $row[2];
-        $dbtelnummer = $row[3];
+      $row = mysqli_fetch_row($result);
+      $gebruikerID = $row[0];
+      $dbemail = $row[1];
+      $dbstraat = $row[2];
+      $dbtelnummer = $row[3];
     }
-  }
-?>
 
-<!DOCTYPE html>
-<html lang="en">
+
+      // In dit gedeelte van de code haal ik alle informatie op
+      // die nodig is om gebruikers aan elkaar te matchen
+      // op basis van de vaardigheden waar ze goed en slecht in zijn
+
+    $query_is_goed_in_categorie = "SELECT Categorie_Categorie
+                                   FROM gebruiker_is_goed_in_categorie
+                                   WHERE Gebruiker_Id = $id";
+
+    $query_is_slecht_in_categorie = "SELECT Categorie_Categorie
+                                     FROM gebruiker_is_slecht_in_categorie
+                                     WHERE Gebruiker_Id = $id";
+
+    $result_is_goed_in_categorie = mysqli_query($connection, $query_is_goed_in_categorie);
+    $result_is_slecht_in_categorie = mysqli_query($connection, $query_is_slecht_in_categorie);
+
+  }
+  ?>
+
+  <!DOCTYPE html>
+  <html lang="en">
   <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -49,6 +66,32 @@
       <p><?php echo($dbtelnummer) ?></p>
 
       <?php
+      $row_count_is_goed_in = mysqli_num_rows($result_is_goed_in_categorie);
+      $row_count_is_slecht_in = mysqli_num_rows($result_is_slecht_in_categorie);
+
+
+      if ($row_count_is_goed_in > 0 && $row_count_is_slecht_in > 0) 
+      {
+        while ($row_goed = $result_is_goed_in_categorie->fetch_assoc()) 
+        {
+              // sla de catogorie op waar de gebruiker goed in is.
+          $is_goed_in_categorie = $row_goed["Categorie_Categorie"];
+          echo "U heeft talent in de volgende categorie: ";
+          echo "<b>";
+          echo ($is_goed_in_categorie);
+          echo "</b>";
+        }
+        echo("<br>");
+        while ($row_slecht = $result_is_slecht_in_categorie->fetch_assoc()) 
+        {
+              // sla de catogorie op waar de gebruiker slecht in is.
+          $is_slecht_in_categorie = $row_slecht["Categorie_Categorie"];
+          echo "U heeft moeite in de volgende categorie: ";
+          echo "<b>";
+          echo ($is_slecht_in_categorie);
+          echo "</b>";
+        }
+
         $sql_get_diensten = "SELECT D.dienst
                              FROM gebruiker_bied_dienst_aan GBDA
                              INNER JOIN dienst D
@@ -73,19 +116,88 @@
           echo "<h3>U biedt momenteel geen diensten aan</h3>";
           echo "<p><a href=\"aanbiedMenu.php\">Klik hier</a> om een dienst toe te voegen<p>";
         }
-      ?>
 
-      <a href="logout.php">Uitloggen</a>
-    </div>
-    
-    <?php else: ?>
-      <h1>Welcome</h1>
-      <a href="login.php">Login</a>
-      <a href="register.php">Register</a>
-    <?php endif; ?>
+        echo("<br>");
 
-    <script src="js/jquery-2.1.4.min.js"></script>
-    <script src="js/bootstrap.min.js"></script> 
-    <script src="js/script.js"></script>
-  </body>
+        $gevonden_match = false;
+
+            //In deze sql query worden mensen uit de database gehaald die hebben aangegeven
+            //dat ze goed zijn in de categorie waar de huidige ingelogde slecht in is.
+        $query_is_goed_in_slecht = "SELECT Gebruiker_Id
+                                    FROM gebruiker_is_goed_in_categorie
+                                    WHERE Categorie_Categorie = '$is_slecht_in_categorie' 
+                                    AND Gebruiker_Id != $id";
+
+        $result_is_goed_in_slecht = mysqli_query($connection, $query_is_goed_in_slecht);         
+
+        while ($row_goed_in_slecht = $result_is_goed_in_slecht->fetch_assoc()) 
+        {
+
+          $match_id_goed_in_slecht = $row_goed_in_slecht["Gebruiker_Id"];
+
+              //In deze sql query worden mensen uit de database gehaald die hebben aangegeven
+              //dat ze slecht zijn in de categorie waar de huidige ingelogde goed in is.
+          $query_is_slecht_in_goed = "SELECT Gebruiker_Id
+                                      FROM gebruiker_is_slecht_in_categorie
+                                      WHERE Categorie_Categorie = '$is_goed_in_categorie' AND Gebruiker_Id != $id
+                                      And Gebruiker_Id = $match_id_goed_in_slecht";
+
+          $result_is_slecht_in_goed = mysqli_query($connection, $query_is_slecht_in_goed);
+
+          while ($row_slecht_in_goed = $result_is_slecht_in_goed->fetch_assoc()) 
+          {
+
+            $match_id_slecht_in_goed = $row_slecht_in_goed["Gebruiker_Id"];
+
+            if ($match_id_slecht_in_goed == $match_id_goed_in_slecht && $gevonden_match == false) 
+            {
+             $gevonden_match = true;
+             echo "<br>";
+             echo "<br>";
+             echo "<h2>Er is een Match!</h2>";
+             echo "<p>Op basis van de door u ingevulde gegevens hebben wij een match gevonden!</p>";
+             echo "<br>";
+
+             $query_match_gebruiker = "SELECT id, email, straat, telefoonnummer
+                                       FROM gebruiker
+                                       WHERE id = $match_id_slecht_in_goed";
+
+             $result_match_gebruiker = mysqli_query($connection, $query_match_gebruiker);
+
+             $match_gebruiker=mysqli_fetch_assoc($result_match_gebruiker);
+
+             echo ($match_gebruiker['email']);
+             echo "<br>";
+             echo ($match_gebruiker['straat']);
+             echo "<br>";
+             echo ($match_gebruiker['telefoonnummer']);
+           }
+         }
+       }
+
+
+       if ($gevonden_match == false)
+       {
+        echo "<h2>Geen match </h2>";
+        echo "<p> Helaas is er op dit moment voor u geen match gevonden. <br>
+        Kijkt u later nog eens om te kijken of er een match is. <br>
+        Nu kunt onder het kopje <a href=\"askForService.php\">dienst vragen</a> mensen vinden die u zouden kunnen helpen</p>";
+      }
+    }
+    ?>
+    <br>
+    <br>
+    <a href="logout.php">Uitloggen</a>
+  </div>
+
+<?php else: ?>
+  <h1>Welcome</h1>
+  <a href="login.php">Login</a>
+  <a href="register.php">Register</a>
+<?php endif; ?>
+
+<script src="js/jquery-2.1.4.min.js"></script>
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/script.js"></script>
+</body>
 </html>
--- Eindproduct/koersWest/register.php
+++ Eindproduct/koersWest/register.php
@@ -9,32 +9,65 @@
 
 	require 'databaseConnectionOpening.php';
 
-	if (!empty($_POST['email']) && !empty($_POST['wachtwoord']))
-	{
-		// Save form data in temporary variables
-		$email = strip_tags($_POST['email']);
-		$wachtwoord = strip_tags($_POST['wachtwoord']);
-		$telnummer = strip_tags($_POST['telnummer']);
-		$straat = strip_tags($_POST['straat']);
-		$postcode = strip_tags($_POST['postcode']);
-		$woonplaats = strip_tags($_POST['woonplaats']);
+	$list_categories = array();
 
-		// Insert form data into the database
-		$query = "INSERT INTO gebruiker (email, wachtwoord, telefoonnummer, straat, postcode, woonplaats)
-			  	  VALUES ('$email', '$wachtwoord', '$telnummer', '$straat', '$postcode', '$woonplaats')";
+			$query_all_categories = "SELECT Categorie FROM Categorie";
+			$result_all_categories = mysqli_query($connection, $query_all_categories);
 
-		$result = mysqli_query($connection, $query);
+			while ($row_categories = $result_all_categories->fetch_assoc()) {
+    		$list_categories[] = $row_categories['Categorie'];
+    		}
 
-		// Successful registration
-		if ($result)
+		if (!empty($_POST) && !empty($_POST['email']) && !empty($_POST['wachtwoord']) &&
+			!empty($_POST['telnummer']) && !empty($_POST['straat']) &&
+			!empty($_POST['postcode']) && !empty($_POST['woonplaats']))
 		{
-			echo "U staat geregistreerd bij boot.";
+			// Save form data in temporary variables
+			$email = strip_tags($_POST['email']);
+			$wachtwoord = strip_tags($_POST['wachtwoord']);
+			$telnummer = strip_tags($_POST['telnummer']);
+			$straat = strip_tags($_POST['straat']);
+			$postcode = strip_tags($_POST['postcode']);
+			$woonplaats = strip_tags($_POST['woonplaats']);
+			$isGoedIn = ($_POST['isGoedIn']);
+			$isSlechtIn = ($_POST['isSlechtIn']);
+
+			// Insert form data into the database
+			$query = "INSERT INTO gebruiker (email, wachtwoord, telefoonnummer, straat, postcode, woonplaats)
+				  	  VALUES ('$email', '$wachtwoord', '$telnummer', '$straat', '$postcode', '$woonplaats')";
+
+			$result = mysqli_query($connection, $query);
+
+			$query_get_user_id = "SELECT Id FROM gebruiker WHERE email = '$email'";
+			$result_get_user_id = mysqli_query($connection, $query_get_user_id);
+			$get_user_id = mysqli_fetch_assoc($result_get_user_id);
+			$id = $get_user_id['Id'];
+
+			$query_insert_goed_in = "INSERT INTO  gebruiker_is_goed_in_categorie
+				  	 			  	 VALUES ($id, '$isGoedIn')";
+
+			$query_insert_slecht_in = "INSERT INTO  gebruiker_is_slecht_in_categorie
+				  	 			  	   VALUES ($id, '$isSlechtIn')";
+
+			$result_inset_goed_in = mysqli_query($connection, $query_insert_goed_in);
+			$result_inset_slecht_in = mysqli_query($connection, $query_insert_slecht_in);
+
+			if ($result && $result_inset_goed_in && $result_inset_slecht_in)
+			{
+				echo "u staat geregistreed bij KoersWest!";
+			}
+			else
+			{
+				echo "Er is iets mis gegaan tijdens het registreren.";
+			}
 		}
-		else // Failed registration
-		{
-		 	die ("Database query failed". mysql_error($connection));
+		else {
+			if (!empty($_POST)){
+			echo "iets is niet goed ingevult";
+			}
 		}
-	}
+	
+
 ?>
 
 <!DOCTYPE html>
@@ -49,24 +82,52 @@
 	</head>
 
 	<body>
+
 	<h1>Register</h1>
 
 		<!-- Registration form -->
+		<div>
 		<form action="register.php" method="POST">
 
-				<input type="text" placeholder="email" name="email">
+				<input class="form-control" type="text" placeholder="email" name="email">
 
-				<input type"password" placeholder="wachtwoord" name="wachtwoord">
-				<input type"password" placeholder="wachtwoord" name="herhaal_wachtwoord">
+				<input class="form-control" type"password" placeholder="wachtwoord" name="wachtwoord">
+				<input class="form-control" type"password" placeholder="wachtwoord" name="herhaal_wachtwoord">
 
-				<input type="text" placeholder="tel. nummer" name="telnummer">
-				<input type="text" placeholder="straat" name="straat">
-				<input type="text" placeholder="postcode" name="postcode">
-				<input type="text" placeholder="woonplaats" name="woonplaats">
+				<input class="form-control" type="text" placeholder="tel. nummer" name="telnummer">
+				<input class="form-control" type="text" placeholder="straat" name="straat">
+				<input class="form-control" type="text" placeholder="postcode" name="postcode">
+				<input class="form-control" type="text" placeholder="woonplaats" name="woonplaats">
 
-				<input type="submit">
+				<label for="selectCategorieGoedIn">Ik ben goed in</label>
+				<select class="form-control" id="selectCategorieGoedIn" name="isGoedIn">
+				<?php
+					foreach ($list_categories as $row) 
+					{
+        				echo"<option>";
+        				echo ($row);
+        				echo "</option>";
+        			}
+        			
+        		?>	
+				</select>		
+
+				<label for="selectCategorieSlechtIn">Ik ben slecht in</label>
+				<select class="form-control" id="selectCategorieSlechtIn" name="isSlechtIn">
+				<?php
+					foreach ($list_categories as $row) 
+					{
+        				echo"<option>";
+        				echo ($row);
+        				echo "</option>";
+        			}
+        		?>
+				</select>	
+
+				<input type="submit" name="submit">
 
 		</form>
+	</div>
 
 		<!-- Return to the index page -->
 		<a href="index.php">Terug</a>
