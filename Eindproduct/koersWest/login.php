c4266157e8655aaadf4f775f6e9b3b644583cecc
CrystalThistle <g.sickenga@gmail.com>
2016-05-17 12:41:53 +0200

    Login navbar aangepast
    Goede navbar want consistentie is leuk

--- Eindproduct/koersWest/login.php
+++ Eindproduct/koersWest/login.php
@@ -53,7 +53,6 @@
         <title>Inloggen</title>
         <style>
             body {
-                padding-top: 5px;
                 padding-bottom: 60px;
                 font-size: 20px;
                 margin-top: 110px;
@@ -82,57 +81,31 @@
         <link rel="stylesheet" href="css/main.css">
     </head>
     <body>
+      <?php 
+      include("Navigation.php");
+      ?>
+      <div id="section">
+          <form class="form-horizontal col-md-4 col-md-offset-4">
+              Voer uw e-mail en wachtwoord in:
 
+              <input type="text" placeholder="Email" name="email" class="form-control">
+              <input type="password" placeholder="Wachtwoord" name ="wachwoord" class="form-control">
+              <button type="submit" class="btn btn-success">Login</button>
 
-        <div class="container"></div>
-        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-            <div class="container">
-                <h1 style="color:white;">Koerswest</h1>
+          </form>
 
-                <div class="navbar-header">
+          <form>
 
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <a class="navbar-brand" href="index.php">HOME</a>
-                    <a class="navbar-brand" href="login.php">Login</a>
-                    <a class="navbar-brand" href="#">About</a>
-                    <a class="navbar-brand" href="register.php">Register</a>
-                </div>
-                <div id="navbar" class="navbar-collapse collapse">
-                    <form class="navbar-form navbar-right" role="form">
+              <div class="round-button"><div class="round-button-circle"><a href="register.php"> <img src ="http://image005.flaticon.com/1/svg/109/109705.svg"></a></div></div>
 
-                    </form>
-                </div>
-            </div>
-        </nav>
-        <div>
-            <div id="section">
-                <form class="form-horizontal col-md-4 col-md-offset-4">
-                    Voer uw e-mail en wachtwoord in:
+          </form>
+      </div>
 
-                    <input type="text" placeholder="Email" name="email" class="form-control">
-                    <input type="password" placeholder="Wachtwoord" name ="wachwoord" class="form-control">
-                    <button type="submit" class="btn btn-success">Login</button>
-
-                </form>
-
-                <form>
-
-                    <div class="round-button"><div class="round-button-circle"><a href="register.php"> <img src ="http://image005.flaticon.com/1/svg/109/109705.svg"></a></div></div>
-
-                </form>
-            </div>
-
-            <hr>
-        </div>
-
-         <?php
-      //This will output error message's << Waar we het over hebben gehad.
-      if(!empty($message)):
-        echo $message."<br><br>";
-      endif;
+      <?php
+        //This will output error message's << Waar we het over hebben gehad.
+        if(!empty($message)):
+          echo $message."<br><br>";
+        endif;
       ?>
 
         <script src="js/jquery-2.1.4.min.js"></script>
