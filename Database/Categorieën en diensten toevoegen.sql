6fe192fd294824e2eb4f1f11ab605538be198078
CrystalThistle <g.sickenga@gmail.com>
2016-05-21 13:21:26 +0200

    Verbeteringen in categorie/dienst benamingen
    Aangepaste namen van categorieën en diensten zodat ze zoveel mogelijk op
    één regel passen in de app en niet te veel ruimte innemen.

--- Database/Categorieën en diensten toevoegen.sql
+++ Database/Categorieën en diensten toevoegen.sql
@@ -1,11 +1,11 @@
 /** Categorieën **/
 
 INSERT INTO categorie (categorie)
-VALUES ('Woning gerelateerd'),
-	   ('Computer gerelateerde vragen' ),
+VALUES ('Woning'),
+	   ('Computer'),
        ('Administratie'),
-       ('Vragen rondom sollicitatie'),
-       ('Vragen rondom overheid'),
+       ('Solliciteren'),
+       ('Overheid'),
        ('Handwerk'),
        ('Sanitair'),
        ('Vervoer'),
@@ -16,65 +16,65 @@ VALUES ('Woning gerelateerd'),
 /** Diensten **/
 INSERT INTO dienst (dienst, omschijving, Categorie_Categorie)
 VALUES 
-	   ('Hulp bij zoeken woningnet','Hulp bij het zoeken van een woning bij woningnet.','Woning gerelateerd'),
-       ('Reageren op bezichtigingen','Hulp bij het reageren op bezichtigingen m.a.v. een datum prikken.','Woning gerelateerd'),
-       ('Gebruiken van e-mail','Hulp bij het gebruik maken van email.','Computer gerelateerde vragen'),
-       ('Installeren van internet','Hulp bij het leren installeren van internet op de computer of telefoon.','Computer gerelateerde vragen'),
-       ('Gebruiken van social media','Hulp bij het leren grbuiken van social media.','Computer gerelateerde vragen'),
-       ('Hulp bij uitkiezen nieuwe computer','Hulp bij het uitkiezen van een nieuwe computer.','Computer gerelateerde vragen');
+	   ('Woningnet','Hulp bij het zoeken van een woning bij woningnet.','Woning'),
+       ('Bezichtigingen','Hulp bij het reageren op bezichtigingen m.a.v. een datum prikken.','Woning'),
+       ('E-mail gebruiken','Hulp bij het gebruik maken van email.','Computer'),
+       ('Internet configureren','Hulp bij het leren opzetten van een internet verbinding op de computer of telefoon.','Computer'),
+       ('Social media','Hulp bij het leren grbuiken van social media.','Computer'),
+       ('Computer aanschaffen','Hulp bij het uitkiezen van een nieuwe computer.','Computer');
        
 /** Diensten **/
 INSERT INTO dienst (dienst, omschijving, Categorie_Categorie)
 VALUES 
-       ('Virus gerelateerd','Het laten verwijderen van een virus op de computer.','Computer gerelateerde vragen'),
-       ('Basics mobiele telefoon','De basics van de mobiele telfoon zoals een sms versturen of applicaties installeren uitgelegd krijgen.','Computer gerelateerde vragen'),
-       ('Hulp bij lezen van post','Leren hoe U de post moet lezen en waar U op moet letten.','Administratie'),
-       ('Afhandelen van post','Hulp bij het afhandelen van post van bijv. instanties.','Administratie'),
-       ('Bellen van instanties','Leer hoe je een gesprek kan voeren met instanties.','Administratie'),
+       ('Virus verwijderen','Het laten verwijderen van een virus op de computer.','Computer'),
+       ('Mobiele telefoon','De basics van de mobiele telefoon zoals een sms versturen of applicaties installeren uitgelegd krijgen.','Computer'),
+       ('Post lezen','Leren hoe u de post moet lezen en waar u op moet letten.','Administratie'),
+       ('Post afhandelen','Hulp bij het Post afhandelen van bijv. instanties.','Administratie'),
+       ('Instanties bellen','Leer hoe je een gesprek kan voeren met instanties.','Administratie'),
        ('Formulieren invullen','Formulieren laten invullen voor aanvragen.','Administratie'),
-       ('CV-opstellen','Leer hoe U een mooie cv kunt opstellen.','Vragen rondom sollicitatie');
+       ('CV-opstellen','Leer hoe u een mooie cv kunt opstellen.','Solliciteren');
 
 /** Diensten **/
 INSERT INTO dienst (dienst, omschijving, Categorie_Categorie)
 VALUES 
-       ('Reageren op (online) sollicitaties','Leer hoe U kunt reageren op online sollicitaties.','Vragen rondom sollicitatie'),
-       ('Inschrijven bij uitzendbureaus','Laat U inschrijven bij uitendbureaus om zo aan een baan te komen.','Vragen rondom sollicitatie'),
-       ('Doorgeven van verlof','Leer hoe U verlof kan vragen bij U werkgever.','Vragen rondom sollicitatie'),
-       ('Oefenen/Leren sollicitatiegesprek','Leren hoe U een sollicitatiegesprek moet voeren.','Vragen rondom sollicitatie'),
-       ('Aanvragen en activeren DigiD code','Hulp bij aanvragen DigdiD.','Vragen rondom overheid'),
-       ('Gebruik websites met behulp van Digid','Hulp bij gebruik maken van websites waar DigiD vereist is.','Vragen rondom overheid'),
-       ('Belastingdienst/toeslagen','Hulp bij alles rondom de belastingdienst of toeslagen.','Vragen rondom overheid'),
-       ('SVB','Hulp bij alles rondom SVB.','Vragen rondom overheid'),
-       ('Zorgverzekering','Hulp bij inloggen bij de zorgverzekering dmv DigiD.','Vragen rondom overheid'),
-       ('Mijn Overheid','Hulp bij het gebruken maken van MijnOverheid.','Vragen rondom overheid'),
-       ('Timmeren','Leer hoe U veilig en efficient kunt timmeren.','Handwerk'),
-       ('Monteren','Leer hoe U bepaalde objecten kunt monteren.','Handwerk');
+       ('Online solliciteren','Leer hoe u kunt reageren op online sollicitaties.','Solliciteren'),
+       ('Inschrijven uitzendbureau','Laat u Inschrijven uitzendbureau om zo aan een baan te komen.','Solliciteren'),
+       ('Verlof aanvragen','Leer hoe u verlof aan kunt vragen bij uw werkgever.','Solliciteren'),
+       ('Sollicitatiegesprek','Leren hoe u een sollicitatiegesprek moet voeren.','Solliciteren'),
+       ('Aanvragen DigiD','Hulp bij het aanvragen van een DigdiD.','Overheid'),
+       ('Websites met DigiD','Hulp bij gebruik maken van websites waar DigiD vereist is.','Overheid'),
+       ('Belasting','Hulp bij alles rondom de belastingdienst of toeslagen.','Overheid'),
+       ('SVB','Hulp bij alles rondom SVB.','Overheid'),
+       ('Zorgverzekering','Hulp bij inloggen bij de zorgverzekering dmv DigiD.','Overheid'),
+       ('MijnOverheid','Hulp bij het gebruken maken van MijnOverheid.','Overheid'),
+       ('Timmeren','Leer hoe u veilig en efficient kunt timmeren.','Handwerk'),
+       ('Monteren','Leer hoe u bepaalde objecten kunt monteren.','Handwerk');
 
 /** Diensten **/
 INSERT INTO dienst (dienst, omschijving, Categorie_Categorie)
 VALUES 
-       ('Verven','Hulp in het verven zodat U kunt verven zonder spetters op de grond.','Handwerk'),
+       ('Verven','Hulp in het verven zodat u kunt verven zonder spetters op de grond.','Handwerk'),
        ('Verhuizen','Hulp nodig bij het verhuizen.','Handwerk'),
-       ('Tuinieren','Leer hoe U kunt tuinieren zodat uw tuin er verzorgd uit ziet.','Handwerk'),
+       ('Tuinieren','Leer hoe u kunt tuinieren zodat uw tuin er verzorgd uit ziet.','Handwerk'),
        ('Boren','Leer veilig boren.','Handwerk'),
-       ('Kast in elkaar zetten','Een kast in elkaar leren zetten.','Handwerk'),
-       ('Schuur opknappen','U schuur opruimen en aanpassingen maken aan de inrichting.','Handwerk'),
+       ('Kast bouwen','Een kast in elkaar leren zetten.','Handwerk'),
+       ('Schuur opknappen','u schuur opruimen en aanpassingen maken aan de inrichting.','Handwerk'),
        ('Ramen lappen','Hulp bij het lappen van ramen.','Handwerk'),
        ('Knippen','Krijg hulp bij het knippen van uw haar.','Handwerk'),
-       ('Elektriciteit gerelateerd','Hulp bij bedradingen e.d. in uw huis.','Handwerk'),
+       ('Elektriciteit','Hulp bij bedradingen e.d. in uw huis.','Handwerk'),
        ('Afwassen','Hulp bij het afwassen van uw serviesgoed.','Sanitair'),
-       ('Schoonmaken van badkamer en WC','Hulp bij het schoonmaken van uw badkamer en uw WC.','Sanitair'),
-       ('Verwijderen van bacterien dmv hoge druk spuit','Laat uw keuken badkamer of toilet weer stralen.','Sanitair'),
-       ('Openbaarvervoer (bus/trein/metro)','Hulp bij het gebruik maken van openbaar vervoer.','Vervoer'),
+       ('Badkamer schoonmaken','Hulp bij het schoonmaken van uw badkamer en uw WC.','Sanitair'),
+       ('Hogedrukspuit','Laat uw keuken badkamer of toilet weer stralen door met een hogedrukspuit alles grondig schoon te maken.','Sanitair'),
+       ('Openbaar vervoer','Hulp bij het gebruik maken van openbaar vervoer.','Vervoer'),
        ('Fietsen','Hulp in het leren fietsen zodat je zelf veilig met de fiets over straat kan.','Vervoer'),
-       ('Eenvoudige gerechten','Leer eenvoudige gerechten maken zoals bijv pasta/rijst.','Koken'),
-       ('Ingewikkelde gerechten','Leer ingewikkelde gerechten maken voor U bezoek.','Koken'),
+       ('Eenvoudige gerechten','Leer eenvoudige gerechten maken zoals bijv. pasta of rijst.','Koken'),
+       ('Ingewikkelde gerechten','Leer ingewikkelde gerechten maken voor u bezoek.','Koken'),
        ('Ontbijt','Hulp bij een gezond ontbijtje.','Koken'),
        ('Meal-preppen','Eten voor de hele week in bakjes zodat u niet meer hoeft te koken','Koken'),
        ('BBQ','Leer veilig te BBQen.','Koken'),
        ('Vissen','Gezellig samen op pad om een dagje te vissen.','Sociaal'),
-       ('Gezellig kopje thee drinken','Samen lekker kopje thee drinken met gegarandeerd leuke gesprekstof.','Sociaal'),
-       ('Uit eten gaan','Gezellig uit eten gaan met zn 2en of in een groep.','Sociaal'),
+       ('Thee drinken','Samen lekker kopje thee drinken met gegarandeerd leuke gesprekstof.','Sociaal'),
+       ('Uit eten gaan','Gezellig Uit eten gaan met zn 2en of in een groep.','Sociaal'),
        ('Luisterend oor','Het luisterend oor voor uw problemen of juist een gezellig gesprek.','Sociaal'),
        ('Activiteiten','Gezellig samen of in een groepsverband activiteiten houden zoals bowlen of hardlopen.','Sociaal'),
        ('Sporten','Samen elkaar motiveren om je doel te behalen!','Sociaal'),
@@ -95,8 +95,8 @@ VALUES ('Sickenga', 'g.sickenga@gmail.com', 'Gijs', 'Ik ben Gijs en ik hou van i
 /** Diensten aanbieden **/
 INSERT INTO gebruiker_bied_dienst_aan (gebruiker_id, dienst_dienst)
 VALUES ('1', 'Formulieren invullen'),
-	   ('1', 'Installeren van internet'),
-       ('1', 'Hulp bij zoeken woningnet'),
+	   ('1', 'Internet configureren'),
+       ('1', 'Woningnet'),
        ('1', 'Knippen');
        
 /** Diensten aanbieden **/
@@ -110,9 +110,9 @@ VALUES
 /** Diensten aanbieden **/
 INSERT INTO gebruiker_bied_dienst_aan (gebruiker_id, dienst_dienst)
 VALUES
-       ('3', 'Schoonmaken van badkamer en WC'),
+       ('3', 'Badkamer schoonmaken'),
 	   ('3', 'Sporten'),
-	   ('3', 'Verwijderen van bacterien dmv hoge druk spuit');
+	   ('3', 'Hogedrukspuit');
 
 /** Diensten aanbieden **/
 INSERT INTO gebruiker_bied_dienst_aan (gebruiker_id, dienst_dienst)
@@ -125,8 +125,8 @@ VALUES
 /** Diensten aanbieden **/
 INSERT INTO gebruiker_bied_dienst_aan (gebruiker_id, dienst_dienst)
 VALUES
-       ('5', 'Elektriciteit gerelateerd'),
-       ('5', 'Openbaarvervoer (bus/trein/metro)');
+       ('5', 'Elektriciteit'),
+       ('5', 'Openbaar vervoer');
        
 
 
@@ -167,19 +167,19 @@ VALUES ('1', 'Vissen'),
        /** Gebruikers diensten vragen **/
 INSERT INTO gebruiker_vraagt_dienst (gebruiker_id, dienst_dienst)
 VALUES
-       ('2', 'Kast in elkaar zetten'),
+       ('2', 'Kast bouwen'),
        ('2', 'Schuur opknappen'),
        ('2', 'Ramen lappen'),
-       ('2', 'Elektriciteit gerelateerd'),
+       ('2', 'Elektriciteit'),
        
-       ('3', 'Openbaarvervoer (bus/trein/metro)'),
+       ('3', 'Openbaar vervoer'),
        ('3', 'Eenvoudige gerechten'),
        ('3', 'Meal-preppen'),
        
-       ('4', 'Bellen van instanties'),
+       ('4', 'Instanties bellen'),
        ('4', 'Formulieren invullen'),
-       ('4', 'Mijn Overheid'),
+       ('4', 'MijnOverheid'),
        
-       ('5', 'Schoonmaken van Badkamer en WC'),
+       ('5', 'Badkamer schoonmaken'),
 	   ('5', 'Afwassen'),
        ('5', 'Ramen lappen');
