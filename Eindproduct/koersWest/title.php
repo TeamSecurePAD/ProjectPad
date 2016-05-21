e0c473c250064c11b729232a7abb9e7b7576abea
CrystalThistle <g.sickenga@gmail.com>
2016-05-22 00:40:32 +0200

    Title linkt naar home.php i.p.v. index.php
    Fix die ik eerder wou committen maar vergeten was, bij deze dus.

--- Eindproduct/koersWest/title.php
+++ Eindproduct/koersWest/title.php
@@ -1,8 +1,8 @@
 <!-- Body div that contains the title of the app - lighter gray backdrop than page background for emphasis on section -->
 <div class = "header">
 	<div class = "subheader">
-	  <!-- Green band to indicate content section containing the actual title of the app -->
-	  <a href = "index.php">
+	  <!-- Gray band to indicate content section containing the actual title of the app -->
+	  <a href = "home.php">
 	  <div class = "subheadertitle">
 	    <span title = "Terug naar home"><h1><c class = "white">KoersWest</c></h1></span>
 	  </div>
