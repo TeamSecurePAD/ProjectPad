487b7e009651e7e0401fcfd8e3c1178d201b28b2
CrystalThistle <g.sickenga@gmail.com>
2016-05-22 16:43:54 +0200

    Home.php nu homepage - Inlog pagina in nieuwe stijl
    Home.php is de nieuwe homepage. Na uitloggen keert de gebruiker hier
    terug.
    
    Nieuwe CSS classes toegevoegd voor forms en error messages op inlog
    pagina. Inlog pagina nu in nieuwe stijl weergegeven.
    
    Home pagina geeft de gebruiker de optie om in te loggen of registreren.
    
    Kleine aanpassingen in bewoordingen op back buttons in dienst
    aanbieden/vragen.

--- Eindproduct/koersWest/home.php
+++ Eindproduct/koersWest/home.php
@@ -10,10 +10,6 @@
     // User's session id, used to reference currently logged in user in database queries
     $id = $_SESSION['user_id'];
   }
-  else // Return to the welcome page
-  {
-    header('location:index.php');
-  }
 ?>
 
 <!DOCTYPE html>
@@ -37,112 +33,196 @@
     <div class = "container">
       <!-- Body div that contains all elements of the page - lighter gray backdrop than page background for emphasis on section -->
       <div class = "body col-xs-12 col-sm-12 col-md-12 col-lg-12">
-        <!-- Title div that surrounds colored title band - white backdrop to further emphasize subsection -->
-        <div class = "title">
-          <!-- Grey band to indicate content section containing the actual title elements -->
-          <div class = "tile_primary">
-            <h1><c class = "white">Hoofdmenu</c></h1>
-            <h2><c class = "white">Selecteer een optie uit het menu om naar die pagina te gaan.</c></h2>
+
+        <?php
+        // --------------------------------------------------------- //
+        // HOME PAGE -> MAIN MENU - DISPLAYED WHEN USER IS LOGGED IN
+        // --------------------------------------------------------- //
+        // MENU OPTIONS:
+        //   * PROFILE
+        //   * OFFER SERVICE
+        //   * ASK FOR SERVICE
+        //   * MATCHING MENU
+        //   * LOGOUT
+        // -------------------------------------------- //
+        if (isset($_SESSION['user_id']))
+        {
+          ?>
+          <!-- Title div that surrounds colored title band - white backdrop to further emphasize subsection -->
+          <div class = "title">
+            <!-- Deep blue band to indicate content section containing the actual title elements -->
+            <div class = "tile_primary">
+              <h1><c class = "white">Hoofdmenu</c></h1>
+              <h2><c class = "white">Selecteer een optie uit het menu om naar die pagina te gaan.</c></h2>
+            </div>
           </div>
-        </div>
 
-        <!-- Subbody div indicates main area of user interaction and important content -->
-        <div class = "subbody">
+          <!-- Subbody div indicates main area of user interaction and important content -->
+          <div class = "subbody">
 
-          <div class = "tile_primary">
-            <h3><b class = "white">Pagina's</b></h3>
+            <div class = "tile_primary">
+              <h3><b class = "white">Pagina's</b></h3>
+            </div>
+
+              <!-- Profile page block -->
+              <div class = "block col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                <div class = "block_primary_small">
+
+                  <!-- Block text -->
+                  <div class = "media-body">
+                    <h3 class = "media-heading"><b class = "white">Mijn profiel</b></h3>
+                    <img class = "image" src = "images/profile.png" width = "150" height = "150"><br><br>
+                  </div>
+
+                  <!-- Submit button -->
+                  <form action = "index.php">
+                    <button type = "submit" class = "btn btn-primary">Bekijk uw profiel</button>
+                  </form>
+                </div>
+              </div>
+              <!-- End of block -->
+
+              <!-- Offer-service block -->
+              <div class = "block col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                <div class = "block_primary_small">
+
+                  <!-- Block text -->
+                  <div class = "media-body">
+                    <h3 class = "media-heading"><b class = "white">Dienst aanbieden</b></h3>
+                    <img class = "image" src = "images/offer_service.png" width = "150" height = "150"><br><br>
+                  </div>
+
+                  <!-- Submit button -->
+                  <form action = "aanbiedMenu.php">
+                    <button type = "submit" class = "btn btn-primary">Bied een dienst aan</button>
+                  </form>
+                </div>
+              </div>
+              <!-- End of block -->
+
+              <!-- Ask-for-service block -->
+              <div class = "block col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                <div class = "block_primary_small">
+
+                  <!-- Block text -->
+                  <div class = "media-body">
+                    <h3 class = "media-heading"><b class = "white">Dienst vragen</b></h3>
+                    <img class = "image" src = "images/ask_for_service.png" width = "150" height = "150"><br><br>
+                  </div>
+
+                  <!-- Submit button -->
+                  <form action = "askForService.php">
+                    <button type = "submit" class = "btn btn-primary">Vraag om een dienst</button>
+                  </form>
+                </div>
+              </div>
+              <!-- End of block -->
+
+              <!-- Matching block -->
+              <div class = "block col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                <div class = "block_primary_small">
+
+                  <!-- Block text -->
+                  <div class = "media-body">
+                    <h3 class = "media-heading"><b class = "white">Matching</b></h3>
+                    <img class = "image" src = "images/handshake_2.png" width = "150" height = "150"><br><br>
+                  </div>
+
+                  <!-- Submit button -->
+                  <form action = "matchNavigationMenu.php">
+                    <button type = "submit" class = "btn btn-primary">Bekijk uw matches</button>
+                  </form>
+                </div>
+              </div>
+              <!-- End of block -->
+
+              <!-- Matching block -->
+              <div class = "block col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                <div class = "block_grey_small">
+
+                  <!-- Block text -->
+                  <div class = "media-body">
+                    <h3 class = "media-heading"><b class = "white">Uitloggen</b></h3>
+                    <img class = "image" src = "images/logout.png" width = "150" height = "150"><br><br>
+                  </div>
+
+                  <!-- Submit button -->
+                  <form action = "logout.php">
+                    <button type = "submit" class = "btn btn-secondary" style = "color: black;">Klik hier om uit te loggen</button>
+                  </form>
+                </div>
+              </div>
+              <!-- End of block -->
+          </div>
+        <?php
+        }
+        // -------------------------------------------------------------------- //
+        // HOME PAGE -> WELCOME PAGE - DISPLAYED WHEN USER IS NOT LOGGED IN YET
+        // -------------------------------------------------------------------- //
+        // MENU OPTIONS:
+        //   * LOGIN
+        //   * REGISTER
+        // -------------------------------------------------------------------- //
+        else
+        {
+          ?>
+          <!-- Title div that surrounds colored title band - white backdrop to further emphasize subsection -->
+          <div class = "title">
+            <!-- Deep blue band to indicate content section containing the actual title elements -->
+            <div class = "tile_primary">
+              <h1><c class = "white">Welkom bij KoersWest!</c></h1>
+              <h2><c class = "white">Log in om verder te gaan naar de applicatie of registreer een nieuw account.</c></h2>
+            </div>
           </div>
 
-            <!-- Profile page block -->
-            <div class = "block col-xs-12 col-sm-6 col-md-4 col-lg-3">
-              <div class = "block_primary_small">
+          <!-- Subbody div indicates main area of user interaction and important content -->
+          <div class = "subbody">
 
-                <!-- Block text -->
-                <div class = "media-body">
-                  <h3 class = "media-heading"><b class = "white">Mijn profiel</b></h3>
-                  <img class = "image" src = "images/profile.png" width = "150" height = "150"><br><br>
-                </div>
-
-                <!-- Submit button -->
-                <form action = "index.php">
-                  <button type = "submit" class = "btn btn-primary">Ga naar uw profiel</button>
-                </form>
-              </div>
+            <div class = "tile_primary">
+              <h3><b class = "white">Opties</b></h3>
             </div>
-            <!-- End of block -->
 
-            <!-- Offer-service block -->
-            <div class = "block col-xs-12 col-sm-6 col-md-4 col-lg-3">
-              <div class = "block_primary_small">
+              <!-- Login block -->
+              <div class = "block col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                <div class = "block_primary_small">
 
-                <!-- Block text -->
-                <div class = "media-body">
-                  <h3 class = "media-heading"><b class = "white">Dienst aanbieden</b></h3>
-                  <img class = "image" src = "images/offer_service.png" width = "150" height = "150"><br><br>
+                  <!-- Block text -->
+                  <div class = "media-body">
+                    <h3 class = "media-heading"><b class = "white">Inloggen</b></h3>
+                    <img class = "image" src = "images/login.png" width = "86" height = "86"><br><br>
+                    Op deze pagina kunt u uw gegevens invoeren om in te loggen.<br><br>
+                  </div>
+
+                  <!-- Submit button -->
+                  <form action = "login.php">
+                    <button type = "submit" class = "btn btn-primary">Ga naar inlogscherm</button>
+                  </form>
                 </div>
-
-                <!-- Submit button -->
-                <form action = "aanbiedMenu.php">
-                  <button type = "submit" class = "btn btn-primary">Bied een dienst aan</button>
-                </form>
               </div>
-            </div>
-            <!-- End of block -->
+              <!-- End of block -->
 
-            <!-- Ask-for-service block -->
-            <div class = "block col-xs-12 col-sm-6 col-md-4 col-lg-3">
-              <div class = "block_primary_small">
+              <!-- Register block -->
+              <div class = "block col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                <div class = "block_primary_small">
 
-                <!-- Block text -->
-                <div class = "media-body">
-                  <h3 class = "media-heading"><b class = "white">Dienst vragen</b></h3>
-                  <img class = "image" src = "images/ask_for_service.png" width = "150" height = "150"><br><br>
+                  <!-- Block text -->
+                  <div class = "media-body">
+                    <h3 class = "media-heading"><b class = "white">Registreren</b></h3>
+                    <img class = "image" src = "images/register.png" width = "86" height = "86"><br><br>
+                    Op deze pagina kunt u een nieuw account aanmaken.<br><br>
+                  </div>
+
+                  <!-- Submit button -->
+                  <form action = "register.php">
+                    <button type = "submit" class = "btn btn-primary">Ga naar registratiescherm</button>
+                  </form>
                 </div>
-
-                <!-- Submit button -->
-                <form action = "askForService.php">
-                  <button type = "submit" class = "btn btn-primary">Vraag om een dienst</button>
-                </form>
               </div>
-            </div>
-            <!-- End of block -->
-
-            <!-- Matching block -->
-            <div class = "block col-xs-12 col-sm-6 col-md-4 col-lg-3">
-              <div class = "block_primary_small">
-
-                <!-- Block text -->
-                <div class = "media-body">
-                  <h3 class = "media-heading"><b class = "white">Matching</b></h3>
-                  <img class = "image" src = "images/handshake_2.png" width = "150" height = "150"><br><br>
-                </div>
-
-                <!-- Submit button -->
-                <form action = "matchNavigationMenu.php">
-                  <button type = "submit" class = "btn btn-primary">Ga naar matching menu</button>
-                </form>
-              </div>
-            </div>
-            <!-- End of block -->
-
-            <!-- Matching block -->
-            <div class = "block col-xs-12 col-sm-6 col-md-4 col-lg-3">
-              <div class = "block_grey_small">
-
-                <!-- Block text -->
-                <div class = "media-body">
-                  <h3 class = "media-heading"><b class = "white">Uitloggen</b></h3>
-                  <img class = "image" src = "images/logout.png" width = "150" height = "150"><br><br>
-                </div>
-
-                <!-- Submit button -->
-                <form action = "logout.php">
-                  <button type = "submit" class = "btn btn-secondary" style = "color: black;">Klik hier om uit te loggen</button>
-                </form>
-              </div>
-            </div>
-            <!-- End of block -->
-        </div>
+              <!-- End of block -->
+          </div>
+          <?php
+        }
+        ?>
       </div>
     </div>
     <br>
